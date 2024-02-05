f4e76632b92025a92f4b73d8e8898a4fc07cf4c6
limin Tian <tianlimin2@gmail.com>
2024-02-05 16:37:03 +0000

    answers 1.9-1.12

--- app/front/entrypoint.sh
+++ app/front/entrypoint.sh
@@ -8,4 +8,10 @@ echo "Will start Nginx with the following configuration:"
 echo "  - WS_BACK_URL : $WS_BACK_URL"
 sed -i 's@TR_WS_BACK_URL@'"$WS_BACK_URL"'@' /usr/share/nginx/html/index.html
 
+# Debugging statements
+echo "Contents of /:"
+ls -l /
+echo "Contents of /usr/share/nginx/html:"
+ls -l /usr/share/nginx/html
+
 exec /docker-entrypoint.sh nginx -g "daemon off;"
